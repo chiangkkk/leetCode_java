1eac8171d6862cf4448460ff270f1dfbe73553aa
chiangkai <chiangkk@163.com>
2022-08-20 22:22:31 +0800

    两数之和

--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-# leeCode 题目
+# leetCode Problems
 
-## 1. [两数之和](/src/main/java/org.chiangkai/problem1)
+1. [两数之和](/src/main/java/org.chiangkai/problem1)
--- src/main/java/org/chiangkai/problem1/Solution.java
+++ src/main/java/org/chiangkai/problem1/Solution.java
@@ -1,15 +1,25 @@
 package org.chiangkai.problem1;
 
+import java.util.Arrays;
+
 /**
- *1. 两数之和
- * 给定一个整数数组 nums 和一个整数目标值 target，
- * 请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
- * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
- *
- * 你可以按任意顺序返回答案。
- *
  * @author ChiangKai
  * @date 2022/8/20 22:02
  */
 public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = nums.length - 1; j > i; j--) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
 }
