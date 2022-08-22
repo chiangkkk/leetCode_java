fc662a4c472755d5510d1151920c5de4a0d7c718
chiangkai <chiangkk@163.com>
2022-08-22 23:25:35 +0800

    add problem `add two number`

---
+++ src/main/java/org/chiangkai/problem2/ListNode.java
@@ -0,0 +1,13 @@
+package org.chiangkai.problem2;
+
+/**
+ * @author ChiangKai
+ * @date 2022/8/22 23:22
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
