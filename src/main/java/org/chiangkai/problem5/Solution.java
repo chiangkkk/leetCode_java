23ef0233c3412ae5bf1ab53b3aec050d4a839d80
chiangkai <chiangkk@163.com>
2022-09-03 22:30:39 +0800

    slove problem5

--- src/main/java/org/chiangkai/problem5/Solution.java
+++ src/main/java/org/chiangkai/problem5/Solution.java
@@ -13,30 +13,25 @@ public class Solution {
             return s;
         }
         char[] str = s.toCharArray();
-        // record[0] 起始位置 record[1] 结束位置
-        int[] record = new int[2];
-        for (int i = 0; i < str.length; i++) {
-            i = findLongestParlindrome(str, record, i);
+        int maxLen = 0, start = 0, end = 0;
+        boolean dp[][] = new boolean[str.length][str.length];
+        for (int i = str.length - 1; i >= 0; i--) {
+            for (int j = i; j < str.length; j++) {
+                if (str[i] == str[j]) {
+                    if (j - i == 0 || j - i == 1) {
+                        dp[i][j] = true;
+                    } else if (dp[i + 1][j - 1]) {
+                        dp[i][j] = true;
+                    }
+                }
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    start = i;
+                    end = j;
+                    maxLen = j - i + 1;
+                }
+            }
         }
-        return s.substring(record[0], record[1] + 1);
+        return s.substring(start, end + 1);
     }
-
-    private int findLongestParlindrome(char[] str, int[] record, int low) {
-        int high = low;
-        while (high < str.length - 1 && str[high + 1] == str[low]) {
-            high++;
-        }
-        int ans = high;
-        while (low > 0 && high < str.length - 1 && str[low - 1] == str[high + 1]) {
-            low--;
-            high++;
-        }
-        if (record[1] - record[0] < high - low) {
-            record[0] = low;
-            record[1] = high;
-        }
-        return ans;
-    }
-
 
 }
--- src/main/java/org/chiangkai/problem5/readme.md
+++ src/main/java/org/chiangkai/problem5/readme.md
@@ -73,3 +73,40 @@ public class Solution {
 
 
 ```
+
+#### 动态规划
+
+```java
+
+public class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        if (s.length() == 1) {
+            return s;
+        }
+        char[] str = s.toCharArray();
+        int maxLen = 0, start = 0, end = 0;
+        boolean dp[][] = new boolean[str.length][str.length];
+        for (int i = str.length - 1; i >= 0; i--) {
+            for (int j = i; j < str.length; j++) {
+                if (str[i] == str[j]) {
+                    if (j - i == 0 || j - i == 1) {
+                        dp[i][j] = true;
+                    } else if (dp[i + 1][j - 1]) {
+                        dp[i][j] = true;
+                    }
+                }
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    start = i;
+                    end = j;
+                    maxLen = j - i + 1;
+                }
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+}
+```
