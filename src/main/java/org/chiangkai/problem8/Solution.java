0ee9a4030ab362237dccd133f023fbc1d0351808
chiangkai <chiangkk@163.com>
2022-09-06 22:58:18 +0800

    add problem 8

---
+++ src/main/java/org/chiangkai/problem8/Solution.java
@@ -0,0 +1,11 @@
+package org.chiangkai.problem8;
+
+/**
+ * @author ChiangKai
+ * @date 2022/9/6 22:53
+ */
+class Solution {
+    public int myAtoi(String s) {
+        return s.length();
+    }
+}
