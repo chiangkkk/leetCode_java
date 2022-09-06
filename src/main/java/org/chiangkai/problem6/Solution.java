fd33bf118b1d1a8990f5e2900f46d640e769a75e
chiangkai <chiangkk@163.com>
2022-09-06 08:48:04 +0800

    slove proble6

--- src/main/java/org/chiangkai/problem6/Solution.java
+++ src/main/java/org/chiangkai/problem6/Solution.java
@@ -1,5 +1,8 @@
 package org.chiangkai.problem6;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author ChiangKai
  * @date 2022/9/5 22:14
@@ -10,33 +13,20 @@ public class Solution {
         if (numRows == 1 || s.length() == 1) {
             return s;
         }
-        StringBuilder sb = new StringBuilder();
-        //中间没数字的情况
-        if (numRows == 2) {
-            for (int i = 0; i < s.length(); i = i + 2) {
-                sb.append(s.charAt(i));
-            }
-            for (int i = 1; i < s.length(); i = i + 2) {
-                sb.append(s.charAt(i));
-            }
-            return sb.toString();
-        }
+        List<StringBuilder> sbs = new ArrayList<>();
         for (int i = 0; i < numRows; i++) {
-            int addNum = 0;
-            int totalGap = numRows * 2 - 2;
-            // 第一行和最后一行差值都是 2n-2
-            if (i == 0 || i + 1 == numRows) {
-                addNum = totalGap;
-            } else {
-                addNum = i * 2;
-            }
-            System.out.println(addNum);
-            for (int j = i; j < s.length(); j = j + addNum) {
-                sb.append(s.charAt(j));
-                if (addNum != totalGap) {
-                    addNum = totalGap - addNum;
-                }
-            }
+            sbs.add(new StringBuilder());
+        }
+        int flag = -1;
+        int line = 0;
+        for (int i = 0; i < s.length(); i++) {
+            sbs.get(line).append(s.charAt(i));
+            if (line == 0 || line + 1 == numRows) flag = -flag;
+            line +=flag;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (StringBuilder stringBuilder : sbs) {
+            sb.append(stringBuilder);
         }
         return sb.toString();
     }
--- src/main/java/org/chiangkai/problem6/readme.md
+++ src/main/java/org/chiangkai/problem6/readme.md
@@ -98,3 +98,32 @@ public class Solution {
 }
 
 ```
+
+- 学习一波k神的Z字抖动
+```java
+public class Solution {
+    public String convert(String s, int numRows) {
+        //可以直接返回的情况
+        if (numRows == 1 || s.length() == 1) {
+            return s;
+        }
+        List<StringBuilder> sbs = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            sbs.add(new StringBuilder());
+        }
+        int flag = -1;
+        int line = 0;
+        for (int i = 0; i < s.length(); i++) {
+            sbs.get(line).append(s.charAt(i));
+            if (line == 0 || line + 1 == numRows) flag = -flag;
+            line +=flag;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (StringBuilder stringBuilder : sbs) {
+            sb.append(stringBuilder);
+        }
+        return sb.toString();
+    }
+}
+
+```
