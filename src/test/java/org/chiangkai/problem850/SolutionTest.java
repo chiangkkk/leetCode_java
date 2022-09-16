d530042fa1caeb7f629f5dfd16fa84f9caac7dd3
chiangkai <chiangkk@163.com>
2022-09-16 09:35:59 +0800

    update

---
+++ src/test/java/org/chiangkai/problem850/SolutionTest.java
@@ -0,0 +1,14 @@
+package org.chiangkai.problem850;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+    @Test
+    public void test850(){
+        Solution solution = new Solution();
+        solution.rectangleArea(new int[1][1]);
+    }
+
+}
