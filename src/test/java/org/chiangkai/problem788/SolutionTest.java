4e2544f6f3ab1124add2b58858e278b25b44ec6d
chiangkai <chiangkk@163.com>
2022-09-25 20:47:41 +0800

    788

---
+++ src/test/java/org/chiangkai/problem788/SolutionTest.java
@@ -0,0 +1,14 @@
+package org.chiangkai.problem788;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+    @Test
+    public void test788(){
+        Solution solution = new Solution();
+        System.out.println(solution.rotatedDigits((int) 1e4));
+    }
+
+}
