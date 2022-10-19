36bee923361571dfbe571fe0aea724d4fd476ae3
chiangkai <chiangkk@163.com>
2022-10-19 10:30:17 +0800

    update

---
+++ src/main/java/org/chiangkai/problem1700/Solution.java
@@ -0,0 +1,25 @@
+package org.chiangkai.problem1700;
+
+import java.util.Arrays;
+
+/**
+ * @author ChiangKai
+ * @date 2022/10/19
+ */
+public class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int s1 = Arrays.stream(students).sum();
+        int s0 = students.length - s1;
+        for (int i = 0; i < sandwiches.length; i++) {
+            if (sandwiches[i] == 0 && s0 > 0) {
+                s0--;
+            } else if (sandwiches[i] == 1 && s1 > 0) {
+                s1--;
+            } else {
+                break;
+            }
+        }
+        return s1 + s0;
+
+    }
+}
