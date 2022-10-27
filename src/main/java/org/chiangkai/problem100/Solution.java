13e487a0a5a3baf8b1e55b50d66bce4b452c4ef5
chiangkai <chiangkk@163.com>
2022-10-27 17:45:52 +0800

    update

--- src/main/java/org/chiangkai/problem100/Solution.java
+++ src/main/java/org/chiangkai/problem100/Solution.java
@@ -6,20 +6,16 @@ package org.chiangkai.problem100;
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return find(p, q);
-    }
-
-    public boolean find(TreeNode p, TreeNode q) {
         if (p == q) {
             return true;
-        } else if (p != null && q != null) {
+        }
+        if (p != null && q != null) {
             if (p.val == q.val) {
-                if (find(p.left, q.left)) {
-                    return find(p.right, q.right);
-                }
+                return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
             }
         }
         return false;
     }
+
 
 }
