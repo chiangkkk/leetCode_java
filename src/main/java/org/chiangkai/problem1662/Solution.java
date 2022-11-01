c135acc76d71d91d1db59da13d9c9946fab7ba80
chiangkai <chiangkk@163.com>
2022-11-01 09:21:05 +0800

    update

---
+++ src/main/java/org/chiangkai/problem1662/Solution.java
@@ -0,0 +1,27 @@
+package org.chiangkai.problem1662;
+
+/**
+ * 1662. 检查两个字符串数组是否相等
+ * 输入：word1 = ["ab", "c"], word2 = ["a", "bc"]
+ * 输出：true
+ * 解释：
+ * word1 表示的字符串为 "ab" + "c" -> "abc"
+ * word2 表示的字符串为 "a" + "bc" -> "abc"
+ * 两个字符串相同，返回 true
+ *
+ * @author ChiangKai
+ * @date 2022/11/1
+ */
+public class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        for (String s : word1) {
+            sb1.append(s);
+        }
+        for (String s : word2) {
+            sb2.append(s);
+        }
+        return sb1.toString().equals(sb2.toString());
+    }
+}
